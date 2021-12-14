07f48f9738d2c6c6483f954f953148a15c4747c5
steph <sjapodaca1103@gmail.com>
2021-12-14 13:53:38 -0800

    created files

---
+++ Develop/tests/utils/engineer.js
@@ -0,0 +1,20 @@
+const Employee = require('./employee');
+
+class Engineer extends Employee {
+    constructor(github) {
+        super(name, id, email);
+        this.github = username;
+    }
+
+    getGithub() {
+        console.log(this)
+    }
+      
+}
+
+
+
+
+
+
+getRole()—overridden to return 'Engineer'
