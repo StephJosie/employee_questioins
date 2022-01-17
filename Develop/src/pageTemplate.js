ef9b0b0978c3a080fa2201542295d23bba5f74c8
steph <sjapodaca1103@gmail.com>
2022-01-16 18:29:32 -0800

    fixed html

--- Develop/src/pageTemplate.js
+++ Develop/src/pageTemplate.js
@@ -1,7 +1,5 @@
-// create the team
 const generateTeam = team => {
 
-    // create the manager html
     const generateManager = manager => {
         return `
         <div class="card employee-card">
@@ -20,18 +18,17 @@ const generateTeam = team => {
         `;
     };
 
-    // create the html for engineers
     const generateEngineer = engineer => {
         return `
         <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        <h3 class="card-title"><i></i>${engineer.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
             <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">Email: <a href="${engineer.getEmail()}">${engineer.getEmail()}</a></li>
             <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
         </ul>
     </div>
@@ -39,7 +36,7 @@ const generateTeam = team => {
         `;
     };
 
-    // create the html for interns
+
     const generateIntern = intern => {
         return `
         <div class="card employee-card">
@@ -50,7 +47,7 @@ const generateTeam = team => {
     <div class="card-body">
         <ul class="list-group">
             <li class="list-group-item">ID: ${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">Email: <a href="${intern.getEmail()}">${intern.getEmail()}</a></li>
             <li class="list-group-item">School: ${intern.getSchool()}</li>
         </ul>
     </div>
@@ -79,7 +76,6 @@ const generateTeam = team => {
 
 }
 
-// export function to generate entire page
 module.exports = team => {
 
     return `
@@ -94,7 +90,7 @@ module.exports = team => {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    
 </head>
 
 <body>
